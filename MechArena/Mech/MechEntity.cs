56db7393ec413cc23b217abf545f1fcf04386391
MoyTW <moytravis@gmail.com>
2016-12-07 18:55:24 -0800

    Add Test project

---
+++ MechArena/Mech/MechEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MechArena.Mech
+{
+    public class MechEntity
+    {
+        // Should be configurable!
+        private static Dictionary<BodyPartLocations, int> MechTemplate = new Dictionary<BodyPartLocations, int>() {
+            { BodyPartLocations.HEAD, 5 },
+            { BodyPartLocations.TORSO, 15 },
+            { BodyPartLocations.LEFT_ARM, 8 },
+            { BodyPartLocations.RIGHT_ARM, 8 },
+            { BodyPartLocations.LEFT_LEG, 10 },
+            { BodyPartLocations.RIGHT_LEG, 10 }
+        };
+        private Dictionary<BodyPartLocations, BodyPart> bodyParts;
+
+        public MechEntity()
+        {
+            this.bodyParts = new Dictionary<BodyPartLocations, BodyPart>();
+            foreach (KeyValuePair<BodyPartLocations, int> kvp in MechTemplate)
+            {
+                this.bodyParts[kvp.Key] = new BodyPart(kvp.Key, kvp.Value);
+            }
+        }
+
+        public BodyPart InspectPartAt(BodyPartLocations location)
+        {
+            return bodyParts[location];
+        }
+    }
+}
---
+++ MechArenaTests/Mech/MechEntityTests.cs
@@ -0,0 +1,22 @@
+﻿using MechArena.Mech;
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MechArenaTests.Mech
+{
+    [TestClass]
+    public class MechEntityTests
+    {
+        [TestMethod]
+        public void ConstructorBuildsAllBodyPartsTest()
+        {
+            var mech = new MechEntity();
+
+            foreach(BodyPartLocations location in Enum.GetValues(typeof(BodyPartLocations)))
+            {
+                Assert.IsInstanceOfType(mech.InspectPartAt(location), typeof(BodyPart));
+            }
+        }
+    }
+}
