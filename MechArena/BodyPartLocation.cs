4adf44e8b618de933c9c3f4908a5162f2f87c1a3
MoyTW <moytravis@gmail.com>
2016-12-10 15:00:27 -0800

    Add BodyPartLocation Component

---
+++ MechArena/BodyPartLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MechArena
+{
+    public enum BodyPartLocation
+    {
+        HEAD = 0,
+        TORSO,
+        LEFT_ARM,
+        RIGHT_ARM,
+        LEFT_LEG,
+        RIGHT_LEG
+    }
+}
---
+++ MechArena/Component_BodyPartLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MechArena
+{
+    class Component_BodyPartLocation : Component
+    {
+        public BodyPartLocation Location { get; }
+
+        public Component_BodyPartLocation(BodyPartLocation location)
+        {
+            this.Location = location;
+        }
+    }
+}
