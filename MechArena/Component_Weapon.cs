aa15101ab5e991e147b8e109e4859fe9613f75f8
MoyTW <moytravis@gmail.com>
2016-12-12 09:10:07 -0800

    Add Component_Weapon
    Component_Weapon is just a dumb stat block

---
+++ MechArena/Component_Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechArena
+{
+    public enum WeaponSize
+    {
+        SMALL = 0,
+        MEDIUM,
+        LARGE
+    }
+
+    public class Component_Weapon : Component
+    {
+        public WeaponSize Size { get; }
+        private readonly Dictionary<EntityAttributeType, int> WeaponAttributes;
+
+        // TODO: I would like to define a construct which can be read from a file to construct these!
+        // Right now I'll just hard-code them all and feel bad about it.
+        public Component_Weapon(WeaponSize size, int toHit, int maxRange, int damage, int refireTicks)
+        {
+            this.Size = size;
+            this.WeaponAttributes = new Dictionary<EntityAttributeType, int>() {
+                { EntityAttributeType.TO_HIT, toHit },
+                { EntityAttributeType.MAX_RANGE, maxRange },
+                { EntityAttributeType.DAMAGE, damage },
+                { EntityAttributeType.REFIRE_TICKS, refireTicks },
+            };
+        }
+
+        private void HandleQueryEntityAttribute(GameQuery_EntityAttribute q)
+        {
+            if (this.WeaponAttributes.ContainsKey(q.AttributeType))
+                q.AddModifier(this.WeaponAttributes[q.AttributeType], this.Parent);
+        }
+
+        protected override GameQuery _HandleQuery(GameQuery q)
+        {
+            if (q is GameQuery_EntityAttribute)
+                this.HandleQueryEntityAttribute((GameQuery_EntityAttribute)q);
+
+            return q;
+        }
+
+    }
+}
