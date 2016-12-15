59dd052666e16ed994ba40e7d0d2ace85438a264
MoyTW <moytravis@gmail.com>
2016-12-14 18:58:15 -0800

    Create Arena class
    An Arena is where a single fight happens/is replayed

---
+++ MechArena/Arena.cs
@@ -0,0 +1,112 @@
+﻿using RLNET;
+using RogueSharp;
+
+using System;
+using System.Collections.Generic;
+
+namespace MechArena
+{
+    public class Arena
+    {
+        // TODO: Don't literally have Player/Enemy, as two AIs can fight each other!
+        private Entity player;
+        private Entity enemy;
+        private List<Entity> mapEntities;
+        private IMap arenaMap;
+
+        // TODO: Create a "Mech/Map Blueprint" so you don't pass a literal Entity/IMap instance in!
+        public Arena(Entity player, Entity enemy, IMap arenaMap)
+        {
+            this.player = player;
+            this.enemy = enemy;
+            this.mapEntities = new List<Entity>();
+            this.arenaMap = arenaMap;
+        }
+
+        public bool PlaceEntityNear(Entity en, int x, int y)
+        {
+            for (int nx = x - 1; nx <= x + 1; nx++)
+            {
+                for (int ny = y - 1; ny <= y + 1; ny++)
+                {
+                    if (this.arenaMap.IsWalkableAndOpen(nx, ny, this.mapEntities))
+                    {
+                        if (!this.mapEntities.Contains(en))
+                            this.mapEntities.Add(en);
+                        en.AddComponent(new Component_Position(nx, ny, true));
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // TODO: Testing! Don't directly call!
+        public void TryPlayerAttack()
+        {
+            Console.WriteLine("########## ATTACK INFO ##########");
+            var guns = this.player.HandleQuery(new GameQuery_SubEntities(SubEntitiesSelector.WEAPON)).SubEntities;
+            foreach (var gun in guns)
+            {
+                this.player.HandleEvent(new GameEvent_Attack(player, enemy, gun, this.arenaMap));
+            }
+        }
+
+        // TODO: Testing! Don't directly call!
+        public void TryPlayerMove(int dx, int dy)
+        {
+            var position = this.player.HandleQuery(new GameQuery_Position());
+            if (this.arenaMap.IsWalkableAndOpen(position.X + dx, position.Y + dy, mapEntities))
+            {
+                this.player.HandleEvent(new GameEvent_MoveSingle((XDirection)dx, (YDirection)dy));
+            }
+        }
+
+        // I don't know if "blit" is the term to use for this.
+        public void Blit(RLConsole console)
+        {
+            var enemyPosition = (GameQuery_Position)enemy.HandleQuery(new GameQuery_Position());
+
+            // Use RogueSharp to calculate the current field-of-view for the player
+            var position = (GameQuery_Position)player.HandleQuery(new GameQuery_Position());
+            this.arenaMap.ComputeFov(position.X, position.Y, 50, true);
+
+            foreach (var cell in this.arenaMap.GetAllCells())
+            {
+                // When a Cell is in the field-of-view set it to a brighter color
+                if (cell.IsInFov)
+                {
+                    this.arenaMap.SetCellProperties(cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true);
+                    if (cell.IsWalkable)
+                    {
+                        console.Set(cell.X, cell.Y, RLColor.Gray, null, '.');
+                    }
+                    else
+                    {
+                        console.Set(cell.X, cell.Y, RLColor.LightGray, null, '#');
+                    }
+                }
+                // If the Cell is not in the field-of-view but has been explored set it darker
+                else if (cell.IsExplored)
+                {
+                    if (cell.IsWalkable)
+                    {
+                        console.Set(cell.X, cell.Y, new RLColor(30, 30, 30), null, '.');
+                    }
+                    else
+                    {
+                        console.Set(cell.X, cell.Y, RLColor.Gray, null, '#');
+                    }
+                }
+            }
+
+            // Set the player's symbol after the map symbol to make sure it is draw
+            console.Set(position.X, position.Y, RLColor.LightGreen, null, '@');
+
+            if (enemy.TryGetDestroyed().Destroyed)
+                console.Set(enemyPosition.X, enemyPosition.Y, RLColor.LightGreen, null, 'D');
+            else
+                console.Set(enemyPosition.X, enemyPosition.Y, RLColor.LightGreen, null, 'E');
+        }
+    }
+}
--- MechArena/Program.cs
+++ MechArena/Program.cs
@@ -11,48 +11,27 @@ namespace MechArena
         // The screen height and width are in number of tiles
         private static readonly int _screenWidth = 50;
         private static readonly int _screenHeight = 50;
-        // The starting position for the player
-        private static Entity player;
-        private static Entity enemy;
-        // Will be tricky to track the state here (take or drop) - instead track ALL and iterate through?
-        private static List<Entity> mapEntities;
- 
-        private static RLRootConsole _rootConsole;
-        private static IMap _map;
 
-        private static Entity PlaceEntityNear(Entity en, IMap m, int x, int y)
-        {
-            for(int nx = x - 1; nx <= x + 1; nx++)
-            {
-                for (int ny = y - 1; ny <= y + 1; ny++)
-                {
-                    if (m.IsWalkableAndOpen(nx, ny, mapEntities))
-                    {
-                        if (!mapEntities.Contains(en))
-                            mapEntities.Add(en);
-                        return en.AddComponent(new Component_Position(nx, ny, true));
-                    }
-                }
-            }
-            return null;
-        }
+        private static RLRootConsole _rootConsole;
+
+        private static Arena arena;
 
         public static void Main()
         {
-            mapEntities = new List<Entity>();
-            player = EntityBuilder.BuildPlayer();
-            enemy = EntityBuilder.BuildMech("Test Enemy");
+            // Set up a Arena (move this later)
+            Entity player = EntityBuilder.BuildPlayer();
+            Entity enemy = EntityBuilder.BuildMech("Test Enemy");
+            IMap arenaMap = Map.Create(
+                new RogueSharp.MapCreation.CaveMapCreationStrategy<Map>(_screenWidth, _screenHeight, 45, 4, 3));
+            arena = new Arena(player, enemy, arenaMap);
 
-            // Use RogueSharp to create a new cave map the same size as the screen.
-            _map = Map.Create(new RogueSharp.MapCreation.CaveMapCreationStrategy<Map>(_screenWidth, _screenHeight, 45, 4, 3));
-
-            PlaceEntityNear(player, _map, 25, 25);
-            PlaceEntityNear(enemy, _map, 25, 25);
+            arena.PlaceEntityNear(player, 25, 25);
+            arena.PlaceEntityNear(enemy, 25, 25);
 
             // This must be the exact name of the bitmap font file we are using or it will error.
             string fontFileName = "terminal8x8.png";
             // The title will appear at the top of the console window
-            string consoleTitle = "RougeSharp RLNet Tutorial";
+            string consoleTitle = "Mech Arena (I'm good at naming!) (In-Progress)";
             // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
             _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
             // Set up a handler for RLNET's Update event
@@ -61,26 +40,6 @@ namespace MechArena
             _rootConsole.Render += OnRootConsoleRender;
             // Begin RLNET's game loop
             _rootConsole.Run();
-        }
-
-        // TODO: Testing!
-        private static void TryPlayerAttack()
-        {
-            Console.WriteLine("########## ATTACK INFO ##########");
-            var guns = player.HandleQuery(new GameQuery_SubEntities(SubEntitiesSelector.WEAPON)).SubEntities;
-            foreach(var gun in guns)
-            {
-                player.HandleEvent(new GameEvent_Attack(player, enemy, gun, _map));
-            }
-        }
-
-        private static void TryMove(int dx, int dy)
-        {
-            var position = (GameQuery_Position)player.HandleQuery(new GameQuery_Position());
-            if (_map.IsWalkableAndOpen(position.X + dx, position.Y + dy, mapEntities))
-            {
-                player.HandleEvent(new GameEvent_MoveSingle((XDirection)dx, (YDirection)dy));
-            }
         }
 
         // Event handler for RLNET's Update event
@@ -92,43 +51,43 @@ namespace MechArena
                 switch(keyPress.Key)
                 {
                     case RLKey.F:
-                        TryPlayerAttack();
+                        arena.TryPlayerAttack();
                         break;
                     case RLKey.Up:
-                        TryMove(0, -1);
+                        arena.TryPlayerMove(0, -1);
                         break;
                     case RLKey.Down:
-                        TryMove(0, 1);
+                        arena.TryPlayerMove(0, 1);
                         break;
                     case RLKey.Left:
-                        TryMove(-1, 0);
+                        arena.TryPlayerMove(-1, 0);
                         break;
                     case RLKey.Right:
-                        TryMove(1, 0);
+                        arena.TryPlayerMove(1, 0);
                         break;
                     case RLKey.Keypad1:
-                        TryMove(-1, 1);
+                        arena.TryPlayerMove(-1, 1);
                         break;
                     case RLKey.Keypad2:
-                        TryMove(0, 1);
+                        arena.TryPlayerMove(0, 1);
                         break;
                     case RLKey.Keypad3:
-                        TryMove(1, 1);
+                        arena.TryPlayerMove(1, 1);
                         break;
                     case RLKey.Keypad4:
-                        TryMove(-1, 0);
+                        arena.TryPlayerMove(-1, 0);
                         break;
                     case RLKey.Keypad6:
-                        TryMove(1, 0);
+                        arena.TryPlayerMove(1, 0);
                         break;
                     case RLKey.Keypad7:
-                        TryMove(-1, -1);
+                        arena.TryPlayerMove(-1, -1);
                         break;
                     case RLKey.Keypad8:
-                        TryMove(0, -1);
+                        arena.TryPlayerMove(0, -1);
                         break;
                     case RLKey.Keypad9:
-                        TryMove(1, -1);
+                        arena.TryPlayerMove(1, -1);
                         break;
                     default:
                         break;
@@ -140,51 +99,7 @@ namespace MechArena
         private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
         {
             _rootConsole.Clear();
-
-            var enemyPosition = (GameQuery_Position)enemy.HandleQuery(new GameQuery_Position());
-
-            // Use RogueSharp to calculate the current field-of-view for the player
-            var position = (GameQuery_Position)player.HandleQuery(new GameQuery_Position());
-            _map.ComputeFov(position.X, position.Y, 50, true);
-
-            foreach (var cell in _map.GetAllCells())
-            {
-                // When a Cell is in the field-of-view set it to a brighter color
-                if (cell.IsInFov)
-                {
-                    _map.SetCellProperties(cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true);
-                    if (cell.IsWalkable)
-                    {
-                        _rootConsole.Set(cell.X, cell.Y, RLColor.Gray, null, '.');
-                    }
-                    else
-                    {
-                        _rootConsole.Set(cell.X, cell.Y, RLColor.LightGray, null, '#');
-                    }
-                }
-                // If the Cell is not in the field-of-view but has been explored set it darker
-                else if (cell.IsExplored)
-                {
-                    if (cell.IsWalkable)
-                    {
-                        _rootConsole.Set(cell.X, cell.Y, new RLColor(30, 30, 30), null, '.');
-                    }
-                    else
-                    {
-                        _rootConsole.Set(cell.X, cell.Y, RLColor.Gray, null, '#');
-                    }
-                }
-            }
-
-            // Set the player's symbol after the map symbol to make sure it is draw
-            _rootConsole.Set(position.X, position.Y, RLColor.LightGreen, null, '@');
-
-            if (enemy.TryGetDestroyed().Destroyed)
-                _rootConsole.Set(enemyPosition.X, enemyPosition.Y, RLColor.LightGreen, null, 'D');
-            else
-                _rootConsole.Set(enemyPosition.X, enemyPosition.Y, RLColor.LightGreen, null, 'E');
-
-            // Tell RLNET to draw the console that we set
+            arena.Blit(_rootConsole);
             _rootConsole.Draw();
         }
     }
