3ef71611f8d1809b241bedb17d46718971ab8874
MoyTW <moytravis@gmail.com>
2017-01-08 14:13:01 -0800

    Break competitor listing by group
    This kind of falls down in subsequent rounds.

--- MechArena/UI/CompetitorMenu.cs
+++ MechArena/UI/CompetitorMenu.cs
@@ -48,39 +48,60 @@ namespace MechArena.UI
             }
         }
 
+        // http://stackoverflow.com/questions/1396048/c-sharp-elegant-way-of-partitioning-a-list
+        private IEnumerable<List<T>> Partition<T>(IList<T> source, int size)
+        {
+            for (int i = 0; i < Math.Ceiling(source.Count / (Double)size); i++)
+                yield return new List<T>(source.Skip(size * i).Take(size));
+        }
+
         public void Blit(RLConsole console, Schedule_Tournament tournament)
         {
-            int tableWidth = 30;
-            int currentX = 1;
-            int lineStart = 3;
+            int tableWidth = 40;
+            int currentX = 0;
+            int lineStart = 9;
             int line = lineStart;
 
             console.SetBackColor(0, 0, console.Width, console.Height, RLColor.Black);
-            console.Print(console.Width / 2 - 6, 1, "COMPETITOR MENU", RLColor.White);
+            console.Print(console.Width / 2 - 8, 1, "COMPETITOR MENU", RLColor.White);
 
+            var byGroups = this.Partition(tournament.AllCompetitors(), 32);
+
+            int g = 1;
             int i = 1;
-            foreach (var c in tournament.AllCompetitors())
+            foreach (var group in byGroups)
             {
-                var label = i +") " + c.Label;
-                i++;
 
-                if (tournament.IsEliminated(c.CompetitorID))
-                    console.Print(currentX, line, label, RLColor.Red);
-                else
-                    console.Print(currentX, line, label, RLColor.White);
-
+                console.Print(currentX+10, line++, "GROUP " + g++, RLColor.Green);
                 line++;
-                if (line > console.Height - 3)
+
+                foreach (var c in group)
+                {
+                    var label = i + ") " + c.Label;
+                    i++;
+
+                    if (tournament.IsEliminated(c.CompetitorID))
+                        console.Print(currentX+1, line, label, RLColor.Red);
+                    else
+                        console.Print(currentX+1, line, label, RLColor.White);
+
+                    line++;
+                }
+
+                line += 3;
+                if (line > console.Height - 40)
                 {
                     line = lineStart;
                     currentX += tableWidth;
                 }
             }
 
-            line += 3;
-            console.Print(currentX, line, "Inspect", RLColor.White);
-            line += 1;
-            console.Print(currentX, line, "# " + this.selectionField.SelectionString, RLColor.White);
+            console.Print(74, lineStart - 5, "############", RLColor.White);
+            console.Print(74, lineStart - 4, "# Inspect  #", RLColor.White);
+            console.Print(76, lineStart - 3, "# " + this.selectionField.SelectionString, RLColor.LightGreen);
+            console.Print(74, lineStart - 3, "#", RLColor.White);
+            console.Print(85, lineStart - 3, "#", RLColor.White);
+            console.Print(74, lineStart - 2, "############", RLColor.White);
         }
     }
 }
