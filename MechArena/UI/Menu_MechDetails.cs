3d42ee42d35119772e6bcecfa9c59b6106b58c65
MoyTW <moytravis@gmail.com>
2017-01-24 18:25:18 -0800

    Draw Weapons, Mounts in MechDetails

--- MechArena/UI/Menu_MechDetails.cs
+++ MechArena/UI/Menu_MechDetails.cs
@@ -1,5 +1,6 @@
 ﻿using RLNET;
 using System;
+using System.Linq;
 
 namespace MechArena.UI
 {
@@ -40,22 +41,87 @@ namespace MechArena.UI
             }
         }
 
+        private void DrawWeaponsListing(RLConsole console, int x, int y)
+        {
+            console.Print(x, y++, "####################", RLColor.White);
+            console.Print(x, y++, "# WEAPONS LISTING  #", RLColor.White);
+            console.Print(x, y++, "####################", RLColor.White);
+
+            var skeleton = this.mech.GetComponentOfType<Component_MechSkeleton>();
+            foreach (var location in EntityBuilder.MechLocations)
+            {
+                var holsters = skeleton.InspectBodyPart(location)
+                    .TryGetSubEntities(SubEntitiesSelector.HOLSTERS);
+                if (holsters.Count() > 0)
+                {
+                    console.Print(x, y, "#                  #", RLColor.White);
+                    console.Print(x + 2, y++, location.ToString(), RLColor.White);
+                    console.Print(x, y++, "#------------------#", RLColor.White);
+
+                    foreach (var holster in holsters)
+                    {
+                        var mountedEntity = holster.GetComponentOfType<Component_Holster>().InspectHolsteredEntity();
+                        string mountedString;
+                        if (mountedEntity != null)
+                            mountedString = mountedEntity.ToString();
+                        else
+                            mountedString = "";
+
+                        console.Print(x, y, "+                  +", RLColor.White);
+                        console.Print(x + 2, y++, holster.ToString(), RLColor.White);
+                        console.Print(x, y++, "| ^                |", RLColor.White);
+                        console.Print(x, y, "+                  +", RLColor.White);
+                        console.Print(x + 2, y++, mountedString, RLColor.White);
+                        console.Print(x, y++, "|------------------|", RLColor.White);
+                    }
+                }
+            }
+        }
+
+        private void DrawMountsListing(RLConsole console, int x, int y)
+        {
+            console.Print(x, y++, "####################", RLColor.White);
+            console.Print(x, y++, "#  MOUNTS LISTING  #", RLColor.White);
+            console.Print(x, y++, "####################", RLColor.White);
+
+            var skeleton = this.mech.GetComponentOfType<Component_MechSkeleton>();
+            foreach (var location in EntityBuilder.MechLocations)
+            {
+                var mounts = skeleton.InspectBodyPart(location)
+                    .TryGetSubEntities(SubEntitiesSelector.SWAPPABLE_MOUNTS);
+                if (mounts.Count() > 0)
+                {
+                    console.Print(x, y, "#                  #", RLColor.White);
+                    console.Print(x + 2, y++, location.ToString(), RLColor.White);
+                    console.Print(x, y++, "#------------------#", RLColor.White);
+
+                    foreach (var mount in mounts)
+                    {
+                        var mountedEntity = mount.GetComponentOfType<Component_Mount>().InspectMountedEntity();
+                        string mountedString;
+                        if (mountedEntity != null)
+                            mountedString = mountedEntity.ToString();
+                        else
+                            mountedString = "";
+
+                        console.Print(x, y, "+                  +", RLColor.White);
+                        console.Print(x + 2, y++, mount.ToString(), RLColor.White);
+                        console.Print(x, y++, "| ^                |", RLColor.White);
+                        console.Print(x, y, "+                  +", RLColor.White);
+                        console.Print(x + 2, y++, mountedString, RLColor.White);
+                        console.Print(x, y++, "|------------------|", RLColor.White);
+                    }
+                }
+            }
+        }
+
         public void Blit(RLConsole console)
         {
             Drawer_Mech.DrawMechStatus(this.mech, this.statusConsole);
             RLConsole.Blit(this.statusConsole, 0, 0, Menu_Arena.statusWidth, Menu_Arena.statusHeight, console, 0, 0);
 
-            // TODO: Weapons Column
-            int line = Menu_Arena.statusHeight + 1;
-            console.Print(1, line++, "####################", RLColor.White);
-            console.Print(1, line++, "# WEAPONS LISTING  #", RLColor.White);
-            console.Print(1, line++, "####################", RLColor.White);
-
-            // TODO: Mounts Column
-            line = Menu_Arena.statusHeight + 1;
-            console.Print(22, line++, "####################", RLColor.White);
-            console.Print(22, line++, "#  MOUNTS LISTING  #", RLColor.White);
-            console.Print(22, line++, "####################", RLColor.White);
+            this.DrawWeaponsListing(console, 1, Menu_Arena.statusHeight + 1);
+            this.DrawMountsListing(console, 22, Menu_Arena.statusHeight + 1);
         }
     }
 }
