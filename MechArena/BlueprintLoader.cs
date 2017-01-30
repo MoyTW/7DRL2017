a1945f1ab1738b0244cf562d8f6be3aee2b3192d
MoyTW <moytravis@gmail.com>
2017-01-29 17:15:24 -0800

    Add parse test/experiment
    It seems to parse like I would expect it to, without having to write a
    custom parser.
    
    Now I need something that will take the params and match them up to the
    functions, but that'll be in the Blueprint classes, not in the
    loader/parser code.

---
+++ MechArena/BlueprintLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechArena
+{
+    public static class BlueprintLoader
+    {
+        /*public static Entity LoadBlueprint(File f)
+        {
+
+        }*/
+    }
+}
---
+++ MechArenaTests/BlueprintLoaderTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MechArena;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace MechArenaTests
+{
+    [TestClass]
+    public class BlueprintLoaderTest
+    {
+        [TestMethod]
+        public void TestLoading()
+        {
+            // TODO: Put in loader
+            // TODO: Test file in test project
+            string blueprintsFile = "Resources/TestBlueprints.json";
+            string text = System.IO.File.ReadAllText(blueprintsFile);
+
+            var blueprint = JsonConvert.DeserializeObject<List<Blueprint>>(text)[0];
+            Console.WriteLine(blueprint.Label);
+            Console.WriteLine(blueprint.TypeLabel);
+            foreach(var c in blueprint.Components)
+            {
+                Console.WriteLine(c.Class);
+                foreach(var p in c.Params)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+            Assert.AreEqual(blueprint.Label, "a");
+            Assert.AreEqual(blueprint.TypeLabel, "b");
+            Assert.AreEqual(blueprint.Components[0].Class, "Component_Attachable");
+            Assert.AreEqual(blueprint.Components[0].Params["sizeRequired"], "LARGE");
+            Assert.AreEqual(blueprint.Components[1].Class, "Component_Weapon");
+            Assert.AreEqual(blueprint.Components[1].Params["damage"], "5");
+        }
+    }
+}
