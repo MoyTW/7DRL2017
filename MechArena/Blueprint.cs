66e29353285d0581af5c64f43ab6ac1eb29f7086
MoyTW <moytravis@gmail.com>
2017-01-29 17:14:42 -0800

    Add Blueprint class

---
+++ MechArena/Blueprint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechArena
+{
+    public class ComponentBlueprint
+    {
+        public string Class { get; set; }
+        public Dictionary<string, string> Params { get; set; }
+
+        public Component BuildComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Blueprint
+    {
+        public string Label { get; set; }
+        public string TypeLabel { get; set; }
+        public List<ComponentBlueprint> Components { get; set; }
+
+        public Entity BuildEntity()
+        {
+            var e = new Entity(label: this.Label, typeLabel: this.TypeLabel);
+            foreach(ComponentBlueprint c in this.Components)
+            {
+                e.AddComponent(c.BuildComponent());
+            }
+            return e;
+        }
+    }
+}
