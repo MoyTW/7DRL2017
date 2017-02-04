61442c96b49b32ee7f9312d5a949b95a6c464610
MoyTW <moytravis@gmail.com>
2017-02-04 11:34:14 -0800

    Add Condition_CanSee
    There's an implicit derivation in the previous two conditions that "Enemy"
    is the one and only enemy mech. This, of course, could change - in a 2v2
    this would break down completely.

---
+++ MechArena/AI/Condition_CanSee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechArena.AI
+{
+    class Condition_CanSeeEnemy : Condition
+    {
+        public override bool IsMet(Entity self, ArenaState state)
+        {
+            // TODO: This is gonna show up in a lot of conditions, and looks pretty janky.
+            var enemy = state.Mech1 == self ? state.Mech2 : state.Mech1;
+
+            var enemyPos = enemy.TryGetPosition();
+            var attackerPos = self.TryGetPosition();
+            var path = state.ArenaMap.GetCellsAlongLine(attackerPos.X, attackerPos.Y, enemyPos.X, enemyPos.Y);
+
+            return !path.Any(c => !c.IsWalkable);
+        }
+    }
+}
