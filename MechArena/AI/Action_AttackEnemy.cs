8b086d9c364ae45191d335301a119c768e682650
MoyTW <moytravis@gmail.com>
2017-02-08 19:31:07 -0800

    Add MoveTowardsEnemy, AttackEnemy Actions

---
+++ MechArena/AI/Action_AttackEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MechArena.AI
+{
+    class Action_AttackEnemy : Action
+    {
+        public override GameEvent_Command GenerateCommand(GameQuery_Command commandQuery)
+        {
+            Entity target;
+            if (commandQuery.CommandEntity == commandQuery.ArenaState.Mech1)
+                target = commandQuery.ArenaState.Mech2;
+            else
+                target = commandQuery.ArenaState.Mech1;
+
+            return new GameEvent_Attack(commandQuery.ArenaState.CurrentTick, commandQuery.CommandEntity, target,
+                commandQuery.ExecutorEntity, commandQuery.ArenaState.ArenaMap, commandQuery.Rand);
+        }
+    }
+}
+
---
+++ MechArena/AI/Action_MoveTowardsEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MechArena.AI
+{
+    class Action_MoveTowardsEnemy : Action
+    {
+        public override GameEvent_Command GenerateCommand(GameQuery_Command commandQuery)
+        {
+            Entity target;
+            if (commandQuery.CommandEntity == commandQuery.ArenaState.Mech1)
+                target = commandQuery.ArenaState.Mech2;
+            else
+                target = commandQuery.ArenaState.Mech1;
+
+            // TODO: Wow this is awkward!?
+            var commandPos = commandQuery.CommandEntity.TryGetPosition();
+            var commandCell = commandQuery.ArenaState.ArenaMap.GetCell(commandPos.X, commandPos.Y);
+            var targetPos = target.TryGetPosition();
+            var targetCell = commandQuery.ArenaState.ArenaMap.GetCell(targetPos.X, targetPos.Y);
+
+            var shortestPath = commandQuery.ArenaState.ArenaPathFinder.ShortestPath(commandCell, targetCell);
+            var nextCell = shortestPath.CurrentStep;
+
+            return new GameEvent_MoveSingle(commandQuery.CommandEntity, commandQuery.ArenaState.CurrentTick,
+                nextCell.X - commandPos.X, nextCell.Y - commandPos.Y, commandQuery.ArenaState);
+        }
+    }
+}
+
