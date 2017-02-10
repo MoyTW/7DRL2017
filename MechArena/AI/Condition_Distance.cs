aa19d802050f76d5987265ec80a121ac22f87763
MoyTW <moytravis@gmail.com>
2017-02-09 18:56:41 -0800

    Add distance option to Condition_Distance
    This is to support "If you are further away than you weapon's max range,
    DO X" or "If you are in melee range, DO Y" or similar constructs.
    
    However, it's a little problematic in that if I want to randomly pick a
    condition, now I have to cross-product my n different conditions within
    Condition_Distance. Furthermore, it has issues with "Who's the target?"
    
    I'm considering making the Target a piece of state that carries through
    the condition evaluations and is switched by an explicit SingleClause,
    sort of like how OpenGL does the drawing calls, but that's a little
    further off.

--- MechArena/AI/Condition_Distance.cs
+++ MechArena/AI/Condition_Distance.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace MechArena.AI
 {
+    enum DistanceOption
+    {
+        THIS_WEAPON_RANGE
+        // TODO: Implement the following!
+        // MY_LONGEST_RANGE,
+        // ENEMY_LONGEST_RANGE,
+        // MY_OPTIMAL_RANGE,
+        // ENEMY_OPTIMAL_RANGE
+    }
+
     enum ComparisonOperator
     {
         EQUAL,
@@ -15,8 +25,27 @@ namespace MechArena.AI
 
     class Condition_Distance : Condition
     {
-        public int Distance { get; }
+        public DistanceOption Option { get; }
         public ComparisonOperator Operator { get; }
+
+        public Condition_Distance(ComparisonOperator op, DistanceOption option)
+        {
+            this.Operator = op;
+            this.Option = option;
+        }
+
+        private int ResolveOptionDistance(GameQuery_Command commandQuery, Entity target)
+        {
+            switch (this.Option)
+            {
+                case DistanceOption.THIS_WEAPON_RANGE:
+                    var range = commandQuery.ExecutorEntity
+                        .TryGetAttribute(EntityAttributeType.MAX_RANGE, commandQuery.ExecutorEntity);
+                    return range.Value;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
 
         public override bool IsMet(GameQuery_Command commandQuery)
         {
@@ -27,6 +56,8 @@ namespace MechArena.AI
             else
                 target = commandQuery.ArenaState.Mech1;
             
+            int optionDistance = this.ResolveOptionDistance(commandQuery, target);
+
             var targetPos = target.TryGetPosition();
             var selfPos = commandQuery.CommandEntity.TryGetPosition();
             int currDist = commandQuery.ArenaState.ArenaMap
@@ -36,11 +67,11 @@ namespace MechArena.AI
             switch(this.Operator)
             {
                 case ComparisonOperator.EQUAL:
-                    return currDist == this.Distance;
+                    return currDist == optionDistance;
                 case ComparisonOperator.LESS_THAN:
-                    return currDist < this.Distance;
+                    return currDist < optionDistance;
                 case ComparisonOperator.GREATER_THAN:
-                    return currDist > this.Distance;
+                    return currDist > optionDistance;
                 default:
                     throw new InvalidOperationException("Condition_Distance can't handle " + this.Operator);
             }
