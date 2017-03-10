4bca19baa92fde497aabd0af651457972e9d8abb
MoyTW <moytravis@gmail.com>
2017-03-10 11:57:30 -0800

    AI.ActionClause into own file

--- Executor/AI/Guidebook.cs
+++ Executor/AI/Guidebook.cs
@@ -7,38 +7,6 @@ using System.Threading.Tasks;
 namespace Executor.AI
 {
     [Serializable()]
-    class ActionClause
-    {
-        private IEnumerable<Condition> conditions;
-        private AIAction action;
-
-        public ActionClause(IEnumerable<Condition> conditions, AIAction action)
-        {
-            this.conditions = conditions;
-            this.action = action;
-        }
-
-        public bool ShouldTakeAction(GameQuery_Command commandQuery)
-        {
-            if (!this.action.CanExecuteOn(commandQuery))
-                return false;
-            
-            foreach (var c in this.conditions)
-            {
-                if (!c.IsMet(commandQuery))
-                    return false;
-            }
-
-            return true;
-        }
-
-        public CommandStub CommandForQuery(GameQuery_Command commandQuery)
-        {
-            return this.action.GenerateCommand(commandQuery);
-        }
-    }
-
-    [Serializable()]
     public class Guidebook
     {
         private List<ActionClause> builtRules;
